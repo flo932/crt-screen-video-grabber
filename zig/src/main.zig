9f6d8f402ef1b590a6a889ac31b163a1acee0e6e
flo932 <flo932@uxsrv.de>
2024-03-30 22:37:59 +0100

    refactor: try frame by frame ..not working

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -7,29 +7,7 @@ const assert = @import("std").debug.assert;
 const print = std.debug.print;
 
 
-
-
-pub fn main() !void {
-
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
-    const ms:u64 = std.time.ms_per_s/100;
-    print( "ms: {d}", .{ms});
-    
-    var start:i64 = @divFloor(std.time.milliTimestamp(), ms);
-
-    
-    var f = std.ArrayList(u8).init(allocator);
-    defer f.deinit();
-    var p = std.ArrayList(u8).init(allocator);
-    defer p.deinit();
-    var l = std.ArrayList(u8).init(allocator);
-    defer l.deinit();
-
-    const s1:i64 = @divFloor(std.time.milliTimestamp(), ms)-start;
-    start = @divFloor(std.time.milliTimestamp(), ms);
+pub fn Read(allocator:anytype,f:anytype,p:anytype,l:anytype,start_frame:anytype,init:u8) !void {
 
 
     // Get the path
@@ -59,8 +37,10 @@ pub fn main() !void {
     var line2_old:u64=0;
     
     var pix_line:u64 = 0;
-
+    var i:u64 = 0;
+    var i_ok:u8 = 0;
     while(run3){
+        i_ok=0;
         v = file_buffer[k];
         // print("c {d} {any}\n", .{k,v});
         if( v == 'f'){
@@ -75,9 +55,18 @@ pub fn main() !void {
 
         if( v == '0' or v == '1'){
             if( sw == 'f'){
-                if(fps>1){
-                    try f.append(v);
+                if(fps <= start_frame){
+                }else if(fps>1){
+                    if( init == 1){
+                        try f.append(v);
+                    }else{
+                        if(f.items.len < v){
+                            f.items[i] = v;
+                        }
+                    }
+                    i_ok = 1;
                 }
+
                 if( fps_old != v){
                     fps_old=v;
                     if( v == '1'){
@@ -88,14 +77,30 @@ pub fn main() !void {
                 }
             }
             if( sw == 'p'){
-                if(fps>1){
-                    try p.append(v);
+                if(fps <= start_frame){
+                }else if(fps>1){
+                    if( init == 1){
+                        try p.append(v);
+                    }else{
+                        if(p.items.len < v){
+                            p.items[i] = v;
+                        }
+                    }
+                    i_ok = 1;
                 }
                 pix_line += 1;
             }
             if( sw == 'l'){
-                if(fps>1){
-                    try l.append(v);
+                if(fps <= start_frame){
+                }else if(fps>1){
+                    if( init == 1){
+                        try l.append(v);
+                    }else{
+                        if(l.items.len < v){
+                            l.items[i] = v;
+                        }
+                    }
+                    i_ok = 1;
                 }
                 if( line2_old != v){
                     line2_old=v;
@@ -109,13 +114,16 @@ pub fn main() !void {
             }
             // print(": {c} {d} {c}\n", .{sw,k,v});
         }
-
+        if(i_ok==1){
+            i+=1;
+        }
 
         k+=1;
         if(k>file_buffer.len-1){
             run3=false;
         }
         if(fps>2){
+           fps=0;
            break;
         }
     }
@@ -127,7 +135,33 @@ pub fn main() !void {
     print("\n", .{});
 
 
+}
+
+pub fn main() !void {
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const ms:u64 = std.time.ms_per_s/100;
+    print( "ms: {d}", .{ms});
     
+    var start:i64 = @divFloor(std.time.milliTimestamp(), ms);
+
+    
+    var f = std.ArrayList(u8).init(allocator);
+    defer f.deinit();
+    var p = std.ArrayList(u8).init(allocator);
+    defer p.deinit();
+    var l = std.ArrayList(u8).init(allocator);
+    defer l.deinit();
+
+    const s1:i64 = @divFloor(std.time.milliTimestamp(), ms)-start;
+    start = @divFloor(std.time.milliTimestamp(), ms);
+
+    var start_frame:u64=0;
+    _ = try Read(allocator,&f,&p,&l,start_frame,1);
+    start_frame += 1;
 
     const s21:u8 =0;
     const s2:u8 =0;
@@ -166,6 +200,7 @@ pub fn main() !void {
     var line_old:u8 = ' ';
     var line_on: u8 = 0;
     var frame_nr:u64 = 0;
+    var ix:u64 = 0;
     while (!quit) {
         var event: c.SDL_Event = undefined;
         while (c.SDL_PollEvent(&event) != 0) {
@@ -177,7 +212,7 @@ pub fn main() !void {
                 else => {},
             }
         }
-
+        ix = 0;
         _ = c.SDL_RenderClear(renderer);
         _ = c.SDL_RenderDrawLine(renderer ,10,20,30,40);
 
@@ -189,15 +224,28 @@ pub fn main() !void {
         var p5:u8 = ' ';
         var l5:u8 = ' ';
 
-        var ix:u64 = 0;
         var x:i32 = 0;
         var y:i32 = 0;
-
+        
         var line_ok:u8 = 0;
         var rect2 = c.SDL_Rect{ .x = 0, .y = 0, .w = 1, .h = 2 };
+        var rect3 = c.SDL_Rect{ .x = 0, .y = 0, .w = 3, .h = 3 };
         var fps_i:u64=0;
 
         var run5:bool = true;
+
+        // ---------------------
+        //var f = std.ArrayList(u8).init(allocator);
+        //defer f.deinit();
+        //var p = std.ArrayList(u8).init(allocator);
+        //defer p.deinit();
+        //var l = std.ArrayList(u8).init(allocator);
+        //defer l.deinit();
+
+
+        //_ = try Read(allocator,&f,&p,&l,start_frame,0);
+        //start_frame += 100;
+        // ---------------------
 
         while(run5){
             if(ix>=p.items.len){
@@ -217,6 +265,9 @@ pub fn main() !void {
             p5 = p.items[ix];
             l5 = l.items[ix];
 
+            //if( fps_i >= 2 and f5 == '1'){
+            //    break;
+            //}
             if( f5 == '1'){
                 fps_i += 1;
             }
@@ -247,12 +298,21 @@ pub fn main() !void {
             _ = c.SDL_RenderFillRect(renderer, &rect2);
             x+=1;
             ix+=2;
+
+            //rect3.x=30;
+            rect3.y+=10;
+            if(rect3.y > 500){
+                rect3.y = 0;
+                rect3.x+=10;
+            }
+            _ = c.SDL_SetRenderDrawColor(renderer, 0xff, 0xaa, 0xaa, 0xff);
+            _ = c.SDL_RenderFillRect(renderer, &rect3);
         }
 
         c.SDL_RenderPresent(renderer);
 
-        c.SDL_Delay(3); // 0.5 sec
-        print("frame:{}\n", .{frame_nr});
+        c.SDL_Delay(300); // 0.5 sec
+        print("frame:{} {d} {d}\n", .{frame_nr,p.items.len,start_frame});
         frame_nr+=1;
     }
 }
